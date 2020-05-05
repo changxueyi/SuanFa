1996bfc42ed7a57d6d12d53327470afb8bf0ca28
ichangxueyi@163.com <ichangxueyi@163.com>
2020-05-05 08:46:19 +0800

    2020/5/5 08：46

--- src/LeetCode/数组/_21_调整数组顺序奇数位于偶数前面.java
+++ src/LeetCode/数组/_21_调整数组顺序奇数位于偶数前面.java
@@ -24,22 +24,6 @@ public class _21_调整数组顺序奇数位于偶数前面 {
         return nums;
     }
 
-    public int[] exchange1(int[] nums) {
-        //调整顺序，使奇数位于偶数前面，利用双指针技术
-        int left = 0, right = nums.length - 1;
-        while (left != right) {
-            while (left < nums.length && (nums[left] & 1) == 1)
-                left++;
-            while (right >= 0 && (nums[right] & 1) == 0)
-                right--;
-            if (left >= right)
-                break;
-            int mid = nums[left];
-            nums[left] = nums[right];
-            nums[right] = mid;
-        }
-        return nums;
-    }
 
     //Krahets 大佬的解法
     public int[] exchange02(int[] nums) {
